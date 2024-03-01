5e2674c1e5f0792c2eaf3c47e84c1fb513e9d94d
H82M19\Student <Student>
2024-03-01 11:04:58 +0700

    post method

--- server.js
+++ server.js
@@ -14,6 +14,25 @@ app.get('/', (req, res) => {
     res.render('index', {title: title, data: data});
 });
 
+app.post('/save', (req, res) => {
+    const id = data.length + 1;
+    const name = req.body.name;
+    const course_type = req.body.course_type;
+    const course_name = req.body.course_name;
+    const department = req.body.department;
+
+    const newData = {
+        "id": id,
+        "name": name,
+        "course_type": course_type,
+        "course_name": course_name,
+        "department": department
+    }
+    data.push(newData);
+    return res.redirect('/');
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
