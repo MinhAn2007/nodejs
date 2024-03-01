79719dab921564a2720db600d81f310a3fda2bbd
H82M19\Student <Student>
2024-03-01 10:39:58 +0700

    UI

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const title = 'EJS Template Engine';
+
+
+app.use(express.urlencoded({extend: true}));
+app.use(express.static('./templates'));
+
+app.set('view engine', 'ejs');
+app.set('views', './templates');
 
 app.get('/', (req, res) => {  
-  res.send('Hello An!')
+    res.render('index', {title: title, message: 'Hello World!'});
 });
 
 app.listen(port, () => {
